efc705e72ae3f2c3a8d3bececd9eae6b962c933d
berkić <pavel.berka@digital.cz>
2020-11-26 21:38:50 +0100

    [FTR] refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Bootstrap grid examples</h1>
+          <p className="lead">
+            Basic grid layouts to get you familiar with building within the
+            Bootstrap grid system.
+          </p>
+          <p>
+            In these examples the <code>.themed-grid-col</code> class is added
+            to the columns to add some theming. This is not a class that is
+            available in Bootstrap by default.
+          </p>
+
+          <h2 className="mt-4">Five grid tiers</h2>
+          <p>
+            There are five tiers to the Bootstrap grid system, one for each
+            range of devices we support. Each tier starts at a minimum viewport
+            size and automatically applies to the larger devices unless
+            overridden.
+          </p>
+
+          <div className="row mb-3">
+            <div className="col-4 themed-grid-col">.col-4</div>
+            <div className="col-4 themed-grid-col">.col-4</div>
+            <div className="col-4 themed-grid-col">.col-4</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
